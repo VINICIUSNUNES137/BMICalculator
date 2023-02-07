c930c5a18ccff1548f6c61e5d4f6c3115eb4514b
Vinicius Nunes <viniciusnunes137@gmail.com>
2023-02-07 11:21:09 -0300

    07/02
    Criando a interface através do composable

--- app/src/main/java/br/senai/sp/jandira/bmicalculator/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/bmicalculator/MainActivity.kt
@@ -1,8 +1,10 @@
 package br.senai.sp.jandira.bmicalculator
 
+import br.senai.sp.jandira.bmicalculator.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,10 +13,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.bmicalculator.ui.theme.BMICalculatorTheme
 
@@ -23,64 +31,81 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BMICalculatorTheme {
-                Column(
-                    modifier = Modifier
-                        .background(Color.LightGray)
-                        .fillMaxSize(),
-                    verticalArrangement = Arrangement.SpaceBetween,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(
-                        "Hello, world!",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(Color.Black)
-                            .padding(32.dp),
-                        color = Color(red = 3, green = 169, blue = 244, alpha = 255),
-                        fontSize = 32.sp,
-                        textAlign = TextAlign.Center,
-                        fontWeight = FontWeight.Bold
-
-
-                    )
-                    Text(
-                        text = "Vinicius Nunes",
-                        modifier = Modifier.fillMaxWidth(),
-                        color = Color(red = 3, green = 169, blue = 244, alpha = 255),
-                        textAlign = TextAlign.Center,
-                        fontSize = 28.sp,
-                    )
-                    TextField(
-                        value = "", 
-                        onValueChange = {},
-                    label = {
-                        Text(text = "Qual o seu nome?"),
-
-                    })
-                    OutlinedTextField(
-                        value = "",
-                        onValueChange = {},
-                        label = {
-                            Text(text = "Qual o seu CPF?")
-                        },
-                        shape = RoundedCornerShape(20.dp)
-                    )
-                    Row(
-                       modifier = Modifier
-                           .fillMaxWidth()
-                           .padding(top = 16.dp),
-                       horizontalArrangement = Arrangement.SpaceAround
-                   ) {
-                       for(i in 1..3){
-
-                       Button(onClick = { /*TODO*/ }) {
-                           Text(text = "Volta $i",
-                               fontSize = 18.sp)
-                       }
-                   }
-                }
+                CalculatorScreen()
 
             }
         }
     }
-}}
+}
+
+@Preview
+@Composable
+fun CalculatorScreen() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(32.dp)
+        ) {
+            // HEADER
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 32.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.bmi),
+                    contentDescription = "",
+                    modifier = Modifier.size(100.dp)
+                )
+                Text(
+                    text = stringResource(id = R.string.title),
+                    fontSize = 32.sp,
+                    color = Color.Blue,
+                    fontWeight = FontWeight.Bold,
+                    letterSpacing = 4.sp,
+
+                    )
+            }
+            // FORM
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Text(
+                    text = stringResource(id = R.string.weight_label),
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(24.dp)
+                )
+                Text(
+                    text = stringResource(id = R.string.height_label),
+                    modifier = Modifier.padding(top = 8.dp, bottom = 8.dp)
+                )
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(24.dp)
+                )
+                Spacer(modifier = Modifier.height(48.dp))
+                Button(onClick = { /*TODO*/ }, modifier = Modifier
+                    .fillMaxWidth(),
+                    shape = RoundedCornerShape(24.dp)
+                    ) {
+                    Text(text = stringResource(id = R.string.button_calculate),
+                    modifier = Modifier.padding(8.dp),
+                    )
+
+                }
+            }
+
+            // FOOTER
+            Column() {
+
+            }
+        }
+    }
+}
