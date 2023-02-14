685bfa18fca30a6a60cec44704f0c7632fe406f9
Vinicius Nunes <viniciusnunes137@gmail.com>
2023-02-14 10:42:39 -0300

    aceitar digitacao

--- app/src/main/java/br/senai/sp/jandira/bmicalculator/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/bmicalculator/MainActivity.kt
@@ -10,10 +10,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -32,15 +36,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val p1 = Product()
-        p1.id = 100
-        p1.productName = "mouse"
-
-        val c1 = Client(
-            salary = 1500.0,
-            name = "pedro",
-            id = 100
-        )
+//        val p1 = Product()
+//        p1.id = 100
+//        p1.productName = "mouse"
+//
+//        val c1 = Client(
+//            salary = 1500.0,
+//            name = "pedro",
+//            id = 100
+//        )
 
         setContent {
             BMICalculatorTheme {
@@ -54,6 +58,11 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun CalculatorScreen() {
+
+    var weightState = remember {
+        mutableStateOf("")
+    }
+
     Surface(modifier = Modifier.fillMaxSize()) {
         Column(
             modifier = Modifier
@@ -74,21 +83,29 @@ fun CalculatorScreen() {
                 Text(
                     text = stringResource(id = R.string.title),
                     fontSize = 32.sp,
-                    color = Color.Blue,
+                    color = Color(
+                        red = 79,
+                        green =  54,
+                        blue = 232
+                    ),
                     fontWeight = FontWeight.Bold,
                     letterSpacing = 4.sp,
 
                     )
             }
             // FORM
-            Column(modifier = Modifier.fillMaxWidth()) {
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 32.dp, end = 32.dp)) {
                 Text(
                     text = stringResource(id = R.string.weight_label),
                     modifier = Modifier.padding(bottom = 8.dp)
                 )
                 OutlinedTextField(
-                    value = "",
-                    onValueChange = {},
+                    value = weightState.value,
+                    onValueChange = {
+                                    weightState.value = it
+                    },
                     modifier = Modifier.fillMaxWidth(),
                     shape = RoundedCornerShape(24.dp)
                 )
@@ -104,7 +121,8 @@ fun CalculatorScreen() {
                 )
                 Spacer(modifier = Modifier.height(48.dp))
                 Button(onClick = { /*TODO*/ }, modifier = Modifier
-                    .fillMaxWidth().padding(bottom = 32.dp),
+                    .fillMaxWidth()
+                    .padding(bottom = 32.dp),
                     shape = RoundedCornerShape(24.dp)
                     ) {
                     Text(text = stringResource(id = R.string.button_calculate),
@@ -119,13 +137,42 @@ fun CalculatorScreen() {
                 Card(
                    modifier =   Modifier
                         .fillMaxSize(),
+                    shape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp),
                     backgroundColor = Color(
                         red = 79,
                         green =  54,
                         blue = 232
                     )
                 ) {
-
+                    Column(modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                    horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text(
+                            text = stringResource(id = R.string.your_score),
+                            fontSize = 20.sp,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = "0.0",
+                            fontSize = 48.sp,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = stringResource(id = R.string.ideal_state),
+                            color = Color.White
+                        )
+                        Row() {
+                            Button(onClick = { /*TODO*/ }) {
+                                Text(text = stringResource(id = R.string.reset))
+                            }
+                            Spacer(modifier = Modifier.width(24.dp))
+                            Button(onClick = { /*TODO*/ }) {
+                                Text(text = stringResource(id = R.string.share))
+                            }
+                        }
+                    }
 
                 }
             }
